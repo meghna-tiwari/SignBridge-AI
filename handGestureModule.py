0236b77f7eb41236b91d91414b3887e1ce99bc6f
Meghna-tiwari <meghna1@me.iitr.ac.in>
2026-05-29 01:33:29 +0530

    adding new file

---
+++ handGestureModule.py
@@ -0,0 +1,42 @@
+import cv2
+import mediapipe as mp
+import time
+import numpy as np
+
+wCam, hCam=640,480
+
+cap=cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img=cap.read()
+    imgRGB= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                
+                h, w, c = img.shape
+                cx, cy= int(lm.x*w), int(lm.y*h)
+                
+
+                
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) 
+
+
+
+    cTime=time.time()
+    fps = 1/(cTime-pTime)
+    pTime=cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (10,70), cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),3)
+    
+    cv2.imshow("Img",img)
+    cv2.waitKey(1)
