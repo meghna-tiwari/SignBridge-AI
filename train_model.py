7062cf6f79a6c9c3ed212c36d4dcad8e97a9cb0f
megmarock2815 <meghna1@me.iitr.ac.in>
2026-06-04 17:13:11 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -20,8 +20,9 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = RandomForestClassifier(
-    n_estimators=300,
-    random_state=42
+    n_estimators=500,
+    random_state=42,
+    class_weight="balanced"
 )
 
 model.fit(X_train, y_train)
